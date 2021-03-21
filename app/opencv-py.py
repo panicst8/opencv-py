cd3053667bf7c4db513dbf4e888ade6e25238c0d
Chris Milford <panicst8@yahoo.com>
2021-03-21 18:27:09 -0500

    completed more examples

--- app/opencv-py.py
+++ app/opencv-py.py
@@ -1,17 +1,40 @@
 """ opencv thingy """
 
 import cv2
+import numpy as np
 
 img = cv2.imread("assets/finn.jpg")
 
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow("output", img)
-cv2.waitKey(5000)
+imgBlur = cv2.GaussianBlur(img, (7, 7), 0)
 
-cv2.imshow("gray output", imgGray)
+imgCanny = cv2.Canny(img, 100, 100)
 
-cv2.waitKey(5000)
+kernal = np.ones((5, 5), np.uint8)
+
+imgDilation = cv2.dilate(imgCanny, kernal, iterations=2)
+imgEroded = cv2.erode(imgDilation, kernal, iterations=2)
+
+cv2.imshow("Canny output", imgCanny)
+cv2.waitKey(0)
+
+cv2.imshow("dilation output", imgDilation)
+cv2.waitKey(0)
+
+cv2.imshow("eroded output", imgEroded)
+cv2.waitKey(0)
+
+quit()
+
+# cv2.imshow("output", img)
+# cv2.waitKey(0)
+
+# cv2.imshow("blur output", imgBlur)
+# cv2.waitKey(0)
+
+# cv2.imshow("gray output", imgGray)
+# cv2.waitKey(0)
 
 cap = cv2.VideoCapture("assets/finn.mp4")
 
