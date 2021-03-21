1bcb3fe9b1c543e20d3c9f64bfbfa43857ae324a
Chris Milford <panicst8@yahoo.com>
2021-03-21 12:30:50 -0500

    added convert to gray on img

--- app/opencv-py.py
+++ app/opencv-py.py
@@ -4,9 +4,14 @@ import cv2
 
 img = cv2.imread("assets/finn.jpg")
 
-cv2.imshow("output", img)
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-cv2.waitKey(0)
+cv2.imshow("output", img)
+cv2.waitKey(5000)
+
+cv2.imshow("gray output", imgGray)
+
+cv2.waitKey(5000)
 
 cap = cv2.VideoCapture("assets/finn.mp4")
 
@@ -20,6 +25,7 @@ cap = cv2.VideoCapture(0)
 
 cap.set(3, 640)
 cap.set(4, 480)
+cap.set(10, 100)  # brightness setting
 
 while True:
     success, img = cap.read()
