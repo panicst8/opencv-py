02b5c01b19ad56d40d5be6026abb0e618a8d4e4a
Chris Milford <panicst8@yahoo.com>
2021-03-22 09:21:08 -0500

    added cropping

--- app/opencv-py.py
+++ app/opencv-py.py
@@ -4,37 +4,47 @@ import cv2
 import numpy as np
 
 img = cv2.imread("assets/finn.jpg")
+cv2.imshow("output", img)
+cv2.waitKey(0)
 
+# image change to grayscale
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-imgBlur = cv2.GaussianBlur(img, (7, 7), 0)
-
-imgCanny = cv2.Canny(img, 100, 100)
-
-kernal = np.ones((5, 5), np.uint8)
-
-imgDilation = cv2.dilate(imgCanny, kernal, iterations=2)
-imgEroded = cv2.erode(imgDilation, kernal, iterations=2)
-
-cv2.imshow("Canny output", imgCanny)
-cv2.waitKey(0)
-
-cv2.imshow("dilation output", imgDilation)
-cv2.waitKey(0)
-
-cv2.imshow("eroded output", imgEroded)
-cv2.waitKey(0)
-
-quit()
-
-# cv2.imshow("output", img)
+# cv2.imshow("gray output", imgGray)
 # cv2.waitKey(0)
 
+# image blur
+imgBlur = cv2.GaussianBlur(img, (7, 7), 0)
 # cv2.imshow("blur output", imgBlur)
 # cv2.waitKey(0)
 
-# cv2.imshow("gray output", imgGray)
+# resizing image
+imgResize = cv2.resize(img, (300, 200))
+print("img: ", img.shape, "\nimResize: ", imgResize.shape)
+cv2.imshow("resized output", imgResize)
+cv2.waitKey(0)
+
+# crop image
+imgCropped = img[0:500, 200:500]
+cv2.imshow("cropped output", imgCropped)
+cv2.waitKey(0)
+
+
+# imgCanny = cv2.Canny(img, 100, 100)
+# cv2.imshow("Canny output", imgCanny)
 # cv2.waitKey(0)
+
+# kernal = np.ones((5, 5), np.uint8)
+
+# imgDilation = cv2.dilate(imgCanny, kernal, iterations=2)
+# cv2.imshow("dilation output", imgDilation)
+# cv2.waitKey(0)
+
+# imgEroded = cv2.erode(imgDilation, kernal, iterations=2)
+# cv2.imshow("eroded output", imgEroded)
+# cv2.waitKey(0)
+
+quit()
+
 
 cap = cv2.VideoCapture("assets/finn.mp4")
 
