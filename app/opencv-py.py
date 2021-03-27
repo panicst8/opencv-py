5babe1c4595d4cf413acd60082c493b8ac385ca4
Chris Milford <panicst8@yahoo.com>
2021-03-26 19:53:46 -0500

    added skew playing card code

--- app/opencv-py.py
+++ app/opencv-py.py
@@ -4,19 +4,42 @@ import cv2
 import numpy as np
 
 
-img2 = np.zeros((512, 512, 3), np.uint8)
-# print(img2)
+def skew() -> None:
+    """ skew playing card """
 
-# img2[:] = 255, 0, 0 # make image blue
-cv2.line(img2, (10, 10), (100, 100), (0, 255, 0), 5)
-cv2.rectangle(img2, (10, 10), (100, 100), (0, 255, 0), 5)
-cv2.circle(img2, (150, 150), 100, (0, 0, 255), cv2.FILLED)
-cv2.putText(
-    img2, "Hello World", (200, 200), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 255), 2
-)
+    width, height = 250, 350
 
-cv2.imshow("Black Image", img2)
-cv2.waitKey(0)
+    pts_1 = np.float32([[111, 219], [287, 188], [154, 482], [352, 440]])
+    pts_2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
+
+    img = cv2.imread("assets/cards.jpg")
+
+    matrix = cv2.getPerspectiveTransform(pts_1, pts_2)
+    imgOutput = cv2.warpPerspective(img, matrix, (width, height))
+
+    cv2.imshow("cards", imgOutput)
+    cv2.waitKey(0)
+
+
+skew()
+
+quit()
+
+
+def diy_images():
+    img2 = np.zeros((512, 512, 3), np.uint8)
+    # print(img2)
+
+    # img2[:] = 255, 0, 0 # make image blue
+    cv2.line(img2, (10, 10), (100, 100), (0, 255, 0), 5)
+    cv2.rectangle(img2, (10, 10), (100, 100), (0, 255, 0), 5)
+    cv2.circle(img2, (150, 150), 100, (0, 0, 255), cv2.FILLED)
+    cv2.putText(
+        img2, "Hello World", (200, 200), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 255), 2
+    )
+
+    cv2.imshow("Black Image", img2)
+    cv2.waitKey(0)
 
 
 img = cv2.imread("assets/finn.jpg")
