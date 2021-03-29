d1d076f487c886409d076b77019df6cb6d18d457
Chris Milford <panicst8@yahoo.com>
2021-03-28 21:43:15 -0500

    finished color detection

--- app/opencv-py.py
+++ app/opencv-py.py
@@ -1,7 +1,115 @@
 """ opencv thingy """
 
+from os import wait
 import cv2
 import numpy as np
+from typing import Any
+
+
+def stackImages(scale: Any, imgArray: Any) -> Any:
+    """ stack and scale images """
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range(0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
+                    imgArray[x][y] = cv2.resize(
+                        imgArray[x][y], (0, 0), None, scale, scale
+                    )
+                else:
+                    imgArray[x][y] = cv2.resize(
+                        imgArray[x][y],
+                        (imgArray[0][0].shape[1], imgArray[0][0].shape[0]),
+                        None,
+                        scale,
+                        scale,
+                    )
+                if len(imgArray[x][y].shape) == 2:
+                    imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank] * rows
+        hor_con = [imageBlank] * rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(
+                    imgArray[x],
+                    (imgArray[0].shape[1], imgArray[0].shape[0]),
+                    None,
+                    scale,
+                    scale,
+                )
+            if len(imgArray[x].shape) == 2:
+                imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor = np.hstack(imgArray)
+        ver = hor
+    return ver
+
+
+def empty(val: Any) -> None:
+    """ placeholder """
+
+
+def color_detection() -> None:
+    """ detect colors """
+
+    cv2.namedWindow("Trackbars")
+    cv2.resizeWindow("Trackbars", 640, 240)
+    cv2.createTrackbar("Hue Min", "Trackbars", 0, 179, empty)
+    cv2.createTrackbar("Hue Max", "Trackbars", 19, 179, empty)
+    cv2.createTrackbar("Sat Min", "Trackbars", 110, 255, empty)
+    cv2.createTrackbar("Sat Max", "Trackbars", 240, 255, empty)
+    cv2.createTrackbar("Val Min", "Trackbars", 153, 255, empty)
+    cv2.createTrackbar("Val Max", "Trackbars", 255, 255, empty)
+
+    while True:
+        img = cv2.imread("assets/lambo.png")
+        imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        h_min = cv2.getTrackbarPos("Hue Min", "Trackbars")
+        h_max = cv2.getTrackbarPos("Hue Max", "Trackbars")
+        s_min = cv2.getTrackbarPos("Sat Min", "Trackbars")
+        s_max = cv2.getTrackbarPos("Sat Max", "Trackbars")
+        v_min = cv2.getTrackbarPos("Val Min", "Trackbars")
+        v_max = cv2.getTrackbarPos("Val Max", "Trackbars")
+
+        lower = np.array([h_min, s_min, v_min])
+        upper = np.array([h_max, s_max, v_max])
+
+        mask = cv2.inRange(imgHSV, lower, upper)
+
+        imgResult = cv2.bitwise_and(img, img, mask=mask)
+
+        # cv2.imshow("Masked Image", imgResult)
+        # cv2.imshow("Lambo", img)
+        # cv2.imshow("HSV Lambo", imgHSV)
+        # cv2.imshow("Lambo Mask", mask)
+
+        imgStack = stackImages(0.6, ([img, imgHSV], [mask, imgResult]))
+        cv2.imshow("Stacked Images", imgStack)
+
+        cv2.waitKey(1)
+
+
+def img_stacking() -> None:
+    """ use cv2 image stacking functions """
+    img = cv2.imread("assets/finn.jpg")
+
+    imgHor = np.hstack((img, img))
+    imgVir = np.vstack((img, img))
+
+    cv2.imshow("Horizonal img stack", imgHor)
+    cv2.waitKey(0)
+    cv2.imshow("Virtical img stack", imgVir)
+    cv2.waitKey(0)
 
 
 def skew() -> None:
@@ -22,11 +130,6 @@ def skew() -> None:
 
     cv2.imshow("Card skewed", imgOutput)
     cv2.waitKey(0)
-
-
-skew()
-
-quit()
 
 
 def diy_images():
